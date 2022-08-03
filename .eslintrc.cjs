00a6f03cc3a499988fc63a6be4f89c965ddd7c48
Guybrush Threepwood <lucianoumc1@gmail.com>
2022-08-03 19:11:29 -0300

    First Commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:react/jsx-runtime",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/destructuring-assignment": "off",
+    "import/prefer-default-export": "off",
+  },
+};
