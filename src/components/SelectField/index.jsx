87693e9c59e98ac5c60e61ae24acd78fd4029979
Guybrush Threepwood <lucianoumc1@gmail.com>
2022-08-03 20:50:56 -0300

    [CREATE] SelectField component

---
+++ src/components/SelectField/index.jsx
@@ -0,0 +1,17 @@
+export default function SelectField(props) {
+  return (
+    <label htmlFor={props.id} className={`text-sm ${props.className}`}>
+      {props.id}
+      <select
+        {...props}
+        className="p-2 border rounded-lg outline-none text-base w-full focus:shadow-outline-blue"
+        id={props.id}
+      >
+        {props.children}
+      </select>
+      {props.errors && (
+        <div className="text-sm text-[#FF452B]">{`*${props.errors}`}</div>
+      )}
+    </label>
+  );
+}
