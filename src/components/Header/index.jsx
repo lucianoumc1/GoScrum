b35084a7db49a85e195aa78ca91f4a075eac20c7
Guybrush Threepwood <lucianoumc1@gmail.com>
2022-08-18 19:34:28 -0300

    [ADD] add drop down menu with user options

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,50 +1,82 @@
+import { useState, useRef, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-import logOutIcon from "../../assets/icons/log-out.png";
-
 export default function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuRef = useRef(null);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { isLogued } = useSelector((state) => state.authReducer);
+  const {
+    isLogued,
+    currentUser: { userName, teamID },
+  } = useSelector((state) => state.authReducer);
 
   const handleLogout = () => {
     dispatch({ type: "auth/logout" });
     navigate("/login");
   };
 
+  const handleClickOutside = (e) => {
+    if (menuRef.current && !menuRef.current.contains(e.target)) {
+      setOpenMenu(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const clickToCopy = () => {
+    navigator.clipboard.writeText(teamID);
+  };
+
+  const getFirstLetter = (str) => str.charAt(0).toUpperCase();
+
   return (
-    <header className="w-full h-16 p-4 mb-1 shadow-md flex justify-between items-center fixed z-10">
+    <header className="w-full h-16 p-6 mb-1 shadow-md flex justify-between items-center fixed z-10">
       <div className="text-xl font-semibold flex items-center">
-        <span className="text-red-500 text-3xl font-bold ">Go</span>Scrum
+        <span className="text-[#FF452B] text-3xl font-bold ">Go</span>Scrum
       </div>
       {isLogued && (
-        <nav>
-          <ul className="flex gap-4 items-center mr-4">
-            <li className="text-md cursor-pointer bg-">
+        <button
+          type="button"
+          className="bg-[#FF452B] h-8 w-8 block text-white font-bold text-lg text-center rounded-full relative cursor-pointer"
+          onClick={() => setOpenMenu(!openMenu)}
+          ref={menuRef}
+        >
+          {getFirstLetter(userName)}
+          <ul
+            className={`absolute shadow-md overflow-hidden w-40 h-max top-full mt-1 right-0 bg-white border font-semibold text-sm mb-2 text-black rounded-lg border-t-0 ${
+              openMenu ? "block" : "hidden"
+            }`}
+          >
+            <li>
               <a
-                className="w-full p-2 border border-[#FF452B] rounded-lg outline-none bg-[#FF452B] text-white text-sm hover:bg-white hover:text-[#FF452B] hover:border-[#FF452B] transition-hover duration-200"
+                className="w-full py-2 border-[#FF452B] outline-none bg-[#FF452B] text-white text-sm hover:bg-white hover:text-[#FF452B] hover:border-[#FF452B]  transition-hover duration-200"
                 type="button"
-                href="
-              https://mpago.la/1nP2EGd
-              "
+                href="https://mpago.la/1nP2EGd"
                 target="_blank"
                 rel="noreferrer"
               >
-                Donar
+                Hacer donación
               </a>
             </li>
-            <li className="text-md cursor-pointer">
-              <button type="button" onClick={handleLogout} className="h-6 w-6">
-                <img
-                  src={logOutIcon}
-                  alt="log-out"
-                  className="object-contain object-center"
-                />
+            <li className="py-2 hover:bg-[#e5e5e571]">
+              <button type="button" onClick={clickToCopy}>
+                Copiar ID Team
+              </button>
+            </li>
+            <li className="py-2 hover:bg-[#e5e5e571]">
+              <button onClick={handleLogout} type="button">
+                Cerrar sesión
               </button>
             </li>
           </ul>
-        </nav>
+        </button>
       )}
     </header>
   );
