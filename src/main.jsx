de0e14ab4dab8d95eff44c7ae0e10774bacff0b1
Luciano Madroñal <lucianoumc1@gmail.com>
2022-10-25 14:45:24 -0300

    [CHANGE] BroserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store/store";
@@ -6,9 +6,9 @@ import App from "./components/App";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
